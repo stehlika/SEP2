6ff55456004f0ef51c4fc9a286276ba28def535b
adamstehlik <stehlikadam@gmail.com>
2017-05-29 18:49:45 +0200

    Major changes

---
+++ src/Client/Controller/ProfileController.java
@@ -0,0 +1,66 @@
+package Client.Controller;
+
+import Server.Domain.Model.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by adamstehlik on 22/05/2017.
+ */
+public class ProfileController extends MasterController {
+
+    public Button backBtn;
+    public Label nicknameL;
+    public Label playTimeL;
+    public ListView scoresLV;
+    public Label winRatioL;
+    public Label houseL;
+    public ImageView profileIV;
+    public Image image;
+    private ClientRMI rmi = ClientRMI.getInstance();
+
+    public static Player player;
+    private Player _player;
+
+    private ArrayList<Integer> scoresArrayList = new ArrayList<>();
+
+    @FXML
+    public void initialize() {
+        backBtn.toFront();
+        _player = player;
+
+        nicknameL.setText(_player.getNickname());
+        playTimeL.setText(Integer.toString(_player.getPlaytime()) + " s");
+        winRatioL.setText(Double.toString(_player.getWinratio()));
+        houseL.setText(_player.getFaculty());
+        try {
+            scoresArrayList = rmi.getHighScoreForPlayer(_player.getNickname());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (_player.getFaculty().equals("gryffindor")) {
+            image = new Image(getClass().getResourceAsStream("../GameSystem/Resources/harry.png"));
+        } else if (_player.getFaculty().equals("ravenclaw")) {
+            image = new Image(getClass().getResourceAsStream("../GameSystem/Resources/cho.png"));
+        } else if (_player.getFaculty().equals("hufflepuff")) {
+            image = new Image(getClass().getResourceAsStream("../GameSystem/Resources/cedric.png"));
+        } else {
+            image = new Image(getClass().getResourceAsStream("../GameSystem/Resources/draco.png"));
+        }
+        profileIV.setImage(image);
+        manageList(scoresLV, scoresArrayList, "scores");
+    }
+
+    public void back(ActionEvent actionEvent) {
+        backToMainScreen(backBtn, actionEvent);
+    }
+}
---
+++ src/Client/Controller/SortingCeremonyController.java
@@ -0,0 +1,38 @@
+package Client.Controller;
+
+import Server.Domain.Model.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by terezamadova on 24/05/2017.
+ */
+public class SortingCeremonyController extends MasterController {
+
+    public Button continueBtn;
+    public Label houseChosenL;
+    public ImageView houseIV;
+    private String faculty;
+
+    public static Player player;
+    private Player _player;
+
+    @FXML
+    public void initialize() {
+        System.out.println("player"+player);
+        _player = player;
+        System.out.println("_player"+_player);
+        faculty = _player.getFaculty();
+        System.out.println(faculty);
+        continueBtn.toFront();
+        houseChosenL.setText(faculty);
+    }
+
+    public void showMainView(ActionEvent actionEvent) {
+        newView("MainView","../View/mainView.fxml", continueBtn, _player);
+    }
+
+}
